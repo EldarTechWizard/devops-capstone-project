6901ec59be391608293125f7a9b1ebef86be0e9d
EldarTechWizard <eldargonzalez08@gmail.com>
2025-01-29 02:27:21 -0500

    Fixed lint problem

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -9,10 +9,10 @@ import os
 import logging
 from unittest import TestCase
 from tests.factories import AccountFactory
-from service import talisman
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -20,6 +20,8 @@ DATABASE_URI = os.getenv(
 
 BASE_URL = "/accounts"
 
+# To get the Flask test client to use https with the environ_overrides attribute.
+# When making an URL call, pass environ_overrides=HTTPS_ENVIRON
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
 
@@ -36,8 +38,10 @@ class TestAccountService(TestCase):
         app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
-        talisman.force_https = False
         init_db(app)
+        # Talisman will force all requests to your REST API to use the https:// protocol.
+        # This is a good thing, except perhaps when testing (-> all tests will fail).
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -83,7 +87,6 @@ class TestAccountService(TestCase):
         response = self.client.get("/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-
     def test_health(self):
         """It should be healthy"""
         resp = self.client.get("/health")
@@ -91,11 +94,12 @@ class TestAccountService(TestCase):
         data = resp.get_json()
         self.assertEqual(data["status"], "OK")
 
-
-    def test_security_headers(self):
-        """It should return security headers"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+    def test_security_header(self):
+        """Security: The header should contain security information"""
+        response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # For more information about the options of headers:
+        # See Flask Talisman documentation
         headers = {
             'X-Frame-Options': 'SAMEORIGIN',
             'X-Content-Type-Options': 'nosniff',
@@ -105,17 +109,14 @@ class TestAccountService(TestCase):
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
 
-
-    def test_cors_security(self):
-        """It should return a CORS header"""
-        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+    def test_cors_policies(self):
+        """Security: The header should contain the CORS policies"""
+        response = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # Check for the CORS header
-        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
-
+        self.assertEqual(response.headers.get("Access-Control-Allow-Origin"), "*")
 
     def test_create_account(self):
-        """It should Create a new Account"""
+        """Create: It should Create a new Account"""
         account = AccountFactory()
         response = self.client.post(
             BASE_URL,
@@ -136,15 +137,13 @@ class TestAccountService(TestCase):
         self.assertEqual(new_account["phone_number"], account.phone_number)
         self.assertEqual(new_account["date_joined"], str(account.date_joined))
 
-
     def test_bad_request(self):
-        """It should not Create an Account when sending the wrong data"""
+        """Create: It should not Create an Account when sending the wrong data"""
         response = self.client.post(BASE_URL, json={"name": "not enough data"})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-
     def test_unsupported_media_type(self):
-        """It should not Create an Account when sending the wrong media type"""
+        """Create: It should not Create an Account when sending the wrong media type"""
         account = AccountFactory()
         response = self.client.post(
             BASE_URL,
@@ -153,7 +152,6 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
     def test_read_an_account(self):
         """Read: It should Read an Account"""
         account = self._create_accounts(1)[0]
@@ -168,13 +166,11 @@ class TestAccountService(TestCase):
         self.assertEqual(read_account["phone_number"], account.phone_number)
         self.assertEqual(read_account["date_joined"], str(account.date_joined))
 
-
     def test_read_account_not_found(self):
         """Read: It should return error status when no account could be read"""
         invalid_account_id = 0
         response = self.client.get(f"{BASE_URL}/{invalid_account_id}")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
 
     def test_update_an_account(self):
         """Update: It should Update an Account"""
@@ -201,14 +197,12 @@ class TestAccountService(TestCase):
         self.assertEqual(updated_account.phone_number, account.phone_number)
         self.assertEqual(updated_account.date_joined, account.date_joined)
 
-
     def test_update_account_not_found(self):
         """Update: It should return error status when no account could be found"""
         invalid_account_id = 0
         updated_data = {}
         response = self.client.put(f"{BASE_URL}/{invalid_account_id}", json=updated_data)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
 
     def test_update_account_bad_request(self):
         """Update: It should not Update an Account when sending the wrong data"""
@@ -219,7 +213,6 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-
     def test_update_account_unsupported_media_type(self):
         """Update: It should not Update an Account when sending the wrong media type"""
         account = AccountFactory()
@@ -229,7 +222,6 @@ class TestAccountService(TestCase):
             content_type="test/html"
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
-
 
     def test_delete_account(self):
         """Delete: It should Delete an Account"""
@@ -244,13 +236,11 @@ class TestAccountService(TestCase):
         self.assertEqual(Account.find(account.id), None)
         self.assertEqual(len(Account.all()), 0)
 
-
     def test_delete_account_not_found(self):
         """Delete: It should return error status when no account could be found"""
         invalid_account_id = 0
         response = self.client.delete(f"{BASE_URL}/{invalid_account_id}")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-
 
     def test_list_all_accounts(self):
         """List: It should List all Accounts"""
@@ -260,7 +250,6 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         response_data = response.get_json()
         self.assertEqual(len(response_data), account_count)
-
 
     def test_list_all_accounts_no_products_found(self):
         """List: It should return success status when no account could be found"""
